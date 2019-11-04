fc6e231070c351198a72030c4d5e6dae806b1552
Tetiana Reshetnik <reshetniktati@gmail.com>
2019-11-04 18:01:58 +0200

    refactored due to airbnb eslint rules

--- algorithmTasks/555.js
+++ algorithmTasks/555.js
@@ -1,60 +1,55 @@
 const readline = require('readline');
 
 const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-
-rl.question(`Build a Pascal triangle with n rows in it 
-            \nEnter the number of triangle rows: `, (answer) => {
-
-    if (Number(answer)) {
-        console.log(buildTriangle(Number(answer)));
-
-    } else {
-        console.error(`ERROR. Invalid input. '${answer} is not a number.`);
-        process.exit(1);
-    }
-
-    rl.close();
+  input: process.stdin,
+  output: process.stdout,
 });
 
 
 function buildTriangle(n) {
-    let arr = [];
-    const defNumber = 1;
-    let matrix = [];
+  let arr = [];
+  const defNumber = 1;
+  const matrix = [];
 
-    for (let i = 0; i < n; i++) {
-        if (i === 0) {
-            arr = [defNumber];
+  for (let i = 0; i < n; i++) {
+    if (i === 0) {
+      arr = [defNumber];
+    } else {
+      const tempArr = [];
 
+      for (let j = 0; j < arr.length; j++) {
+        if (j === 0) {
+          tempArr.push(defNumber);
         } else {
-            let tempArr = [];
-
-            for (let j = 0; j < arr.length; j++) {
-
-                if (j === 0) {
-                    tempArr.push(defNumber);
-
-                } else {
-                    tempArr.push(arr[j] + arr[j - 1]);
-                }
-
-                if (j === arr.length - 1) {
-                    tempArr.push(defNumber);
-                }
-            }
-            arr = tempArr;
+          tempArr.push(arr[j] + arr[j - 1]);
         }
 
-        let numbersStr = arr.join(' ');
-        let row = new Array(n * 2 - 1).fill(' ');
-        row.splice((n - i), numbersStr.length, ...numbersStr);
+        if (j === arr.length - 1) {
+          tempArr.push(defNumber);
+        }
+      }
+      arr = tempArr;
+    }
 
-        matrix.push(row.join(''));
-        
-    } return matrix.join('\n');
+    const numbersStr = arr.join(' ');
+    const row = new Array(n * 2 - 1).fill(' ');
+    row.splice((n - i), numbersStr.length, ...numbersStr);
+
+    matrix.push(row.join(''));
+  } return matrix.join('\n');
 }
+
+rl.question(`Build a Pascal triangle with n rows in it 
+            \nEnter the number of triangle rows: `, (answer) => {
+  if (Number(answer)) {
+    console.log(buildTriangle(Number(answer)));
+  } else {
+    console.error(`ERROR. Invalid input. '${answer} is not a number.`);
+    process.exit(1);
+  }
+
+  rl.close();
+});
+
 
 module.exports = buildTriangle;
