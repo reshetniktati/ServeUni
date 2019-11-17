f9b3d69f1cb7d8f84880589392937ad52656a8ad
Tetiana Reshetnik <reshetniktati@gmail.com>
2019-11-17 13:52:44 +0200

    tests done

--- Oksanas_tasks_testing/algorithms.js
+++ Oksanas_tasks_testing/algorithms.js
@@ -3,10 +3,7 @@ Given natural numbers. Calculate amount of numbers, which are multiples of 3 and
 
 function calcNatNumbers(str) {
   if (!str) {
-    return 'Input is empty. Enter numbers!'
-  }
-  if (Number(str) < 0) {
-    return 'Invalid input. Enter numbers bigger than 0'
+    return 'Invalid input. Enter numbers bigger than 0!'
   }
   const argsArray = str.split(',').map((el) => {
     return Number(el);
@@ -22,13 +19,8 @@ Given natural numbers. Calculate amount of numbers, which are squares of natural
 
 function numbersAmount(str) {
   if (!str) {
-    return 'Input is empty. Enter numbers!'
+    return 'Invalid input. Enter numbers bigger than 0!'
   }
-
-  if (Number(str) < 0) {
-    return 'Invalid input. Enter numbers bigger than 0'
-  }
-
   const argsArray = str.split(',').map((el) => {
     return Number(el);
   });
@@ -44,34 +36,30 @@ function numbersAmount(str) {
   return calculator
 }
 
-console.log(numbersAmount('-1, 4'))
-
 
 /* task 554
 Given natural number n. Get all the Pythagorean
 triples of natural numbers, each of which does not exceed n.
 a * a + b * b = c * c (a ≤ b ≤ c ≤ n)  */
 function pythagoreanTriplet(n) {
-  if (!n) {
-    return 'Input is empty. Enter numbers!'
-  } 
-  if (isNaN(n)) {
-    return 'Invalid input. Enter number!'
-  }
-  if (n < 0) {
+  if (n === '') {
+    return 'Input is empty. Enter number!'
+  } else if (n <= 0) {
     return 'Invalid input. Enter number bigger than 0'
-  }
-
-  let result = [];
-  for (let a = 1; a < n; a++) {
-    for (let b = a; b < n; b++) {
-      let c = Math.sqrt(a * a + b * b);
-      if (c % 1 === 0 & c <= n) {
-        result.push([a, b, c])
-      }    
-    }
-  }
-  return result
+  } else if (isNaN(n)) {
+    return 'Invalid input. Enter number!'
+  } else {
+    let result = [];
+      for (let a = 1; a < n; a++) {
+        for (let b = a; b < n; b++) {
+          let c = Math.sqrt(a * a + b * b);
+          if (c % 1 === 0 & c <= n) {
+            result.push([a, b, c])
+          }    
+        }
+      }
+    return result
+  } 
 };
 
 
--- Oksanas_tasks_testing/taskstest.test.js
+++ Oksanas_tasks_testing/taskstest.test.js
@@ -2,35 +2,78 @@ const algorithms = require('./algorithms.js');
 
 let str = '1, 2, -1, 4, 5, 0, 7, 8, 9, 16, 36';
 let str1 = 't, 6, dfg, df';
-let str2 = '-1';
 let str3 = 0;
-let str = '';
+let str4 = '';
 let n = 8;
 let n1 = 0;
 let n2 = -1;
-let n3 = ''
-let n4 = 'test'
+let n3 = '';
+let n4 = 'test';
 
-test('is calcNatNumber correct', () => {
-    expect(algorithms.calcNatNumbers(str)).toBe(2)
+
+describe('is caclNatNumber correct', () => {
+
+    test('is calcNatNumber result correct ', () => {
+        expect(algorithms.calcNatNumbers(str)).toBe(2)
+    });
+
+    test('is function choose only numbers', () => {
+        expect(algorithms.calcNatNumbers(str1)).toBe(1)
+    });
+
+    test ('empty input', () => {
+        expect(algorithms.calcNatNumbers(str4)).toBe('Invalid input. Enter numbers bigger than 0!')
+    });
+
+    test ('zero in input', () => {
+        expect(algorithms.calcNatNumbers(str3)).toBe('Invalid input. Enter numbers bigger than 0!')
+    });
+})
+
+describe('is numbersAmount correct', () => {
+
+    test('is numbersAmount result correct', () => {
+        expect(algorithms.numbersAmount(str)).toBe(4)
+    });
+
+    test('input items are numbers in numbersAmount', () => {
+        expect(algorithms.numbersAmount(str1)).toBe(0)
+    });
+
+    test ('empty input', () => {
+        expect(algorithms.calcNatNumbers(str4)).toBe('Invalid input. Enter numbers bigger than 0!')
+    });
+
+    test ('zero in input', () => {
+        expect(algorithms.calcNatNumbers(str3)).toBe('Invalid input. Enter numbers bigger than 0!')
+    });
 });
 
-test('input items are valid in calcNatNumbers', () => {
-    expect(algorithms.calcNatNumbers(str1)).toBe(1)
-});
+describe('is pythagoreanTriplet correct', () => {
+    
+    test('is return value correct', () => {
+        let expected = [[ 3, 4, 5 ]];
+        expect(algorithms.pythagoreanTriplet(n)).toEqual(expect.arrayContaining(expected))
+    });
 
-test('is numbersAmount correct', () => {
-    expect(algorithms.numbersAmount(str)).toBe(4)
-});
+    test('input empty', () => {
+        expect(algorithms.pythagoreanTriplet(n3)).toBe('Input is empty. Enter number!')
+    });
 
-test('input items are valid in numbersAmount', () => {
-    expect(algorithms.numbersAmount(str1)).toBe(0)
-});
+    test('negative input', () => {
+        expect(algorithms.pythagoreanTriplet(n2)).toBe('Invalid input. Enter number bigger than 0')
+    });
 
-test('is pythagoreanTriplet correct', () => {
-    let expected = [[ 3, 4, 5 ]];
-    expect(algorithms.pythagoreanTriplet(n)).toEqual(expect.arrayContaining(expected))
-});
+    test('zero input', () => {
+        expect(algorithms.pythagoreanTriplet(n1)).toBe('Invalid input. Enter number bigger than 0')
+    });
+
+    test('string input', () => {
+        expect(algorithms.pythagoreanTriplet(n4)).toBe('Invalid input. Enter number!')
+    });
+})
+
+
 
 
 
