0d507acd106c83e8fd55f3d6d5f5a9bf7291167a
Tetiana Reshetnik <reshetniktati@gmail.com>
2019-11-17 12:32:22 +0200

    added errors handlers

--- Oksanas_tasks_testing/algorithms.js
+++ Oksanas_tasks_testing/algorithms.js
@@ -1,8 +1,18 @@
 /* task 178b
 Given natural numbers. Calculate amount of numbers, which are multiples of 3 and not multiple of 5 */
 
-function calcNatNumbers(arr) {
-  const counter = arr.filter(num => num % 3 === 0 && num % 5 !== 0).length;
+function calcNatNumbers(str) {
+  if (!str) {
+    return 'Input is empty. Enter numbers!'
+  }
+  if (Number(str) < 0) {
+    return 'Invalid input. Enter numbers bigger than 0'
+  }
+  const argsArray = str.split(',').map((el) => {
+    return Number(el);
+  });
+
+  const counter = argsArray.filter(num => num % 3 === 0 && num % 5 !== 0).length;
   return counter
 };
 
@@ -10,29 +20,55 @@ function calcNatNumbers(arr) {
 /* task 178w
 Given natural numbers. Calculate amount of numbers, which are squares of natural even numbers  */
 
-function numbersAmount(arr) {
-  const evenNaturalNumbers = arr.filter(num => num % 2 === 0);
+function numbersAmount(str) {
+  if (!str) {
+    return 'Input is empty. Enter numbers!'
+  }
+
+  if (Number(str) < 0) {
+    return 'Invalid input. Enter numbers bigger than 0'
+  }
+
+  const argsArray = str.split(',').map((el) => {
+    return Number(el);
+  });
+
+  const evenNaturalNumbers = argsArray.filter(num => num % 2 === 0);
   let calculator = 0;
   evenNaturalNumbers.forEach(function (evenNum) {
     if (Math.sqrt(evenNum) % 2 === 0) {
       calculator++;
     }
-  })
+  });
+  
   return calculator
 }
+
+console.log(numbersAmount('-1, 4'))
+
 
 /* task 554
 Given natural number n. Get all the Pythagorean
 triples of natural numbers, each of which does not exceed n.
 a * a + b * b = c * c (a ≤ b ≤ c ≤ n)  */
 function pythagoreanTriplet(n) {
+  if (!n) {
+    return 'Input is empty. Enter numbers!'
+  } 
+  if (isNaN(n)) {
+    return 'Invalid input. Enter number!'
+  }
+  if (n < 0) {
+    return 'Invalid input. Enter number bigger than 0'
+  }
+
   let result = [];
   for (let a = 1; a < n; a++) {
     for (let b = a; b < n; b++) {
       let c = Math.sqrt(a * a + b * b);
       if (c % 1 === 0 & c <= n) {
         result.push([a, b, c])
-      }
+      }    
     }
   }
   return result
--- Oksanas_tasks_testing/taskstest.test.js
+++ Oksanas_tasks_testing/taskstest.test.js
@@ -1,23 +1,30 @@
 const algorithms = require('./algorithms.js');
 
-let arr = [1, 2, -1, 4, 5, 0, 7, 8, 9, 16, 36];
-let arr1 = ['t', 6, 'dfg', 'df'];
+let str = '1, 2, -1, 4, 5, 0, 7, 8, 9, 16, 36';
+let str1 = 't, 6, dfg, df';
+let str2 = '-1';
+let str3 = 0;
+let str = '';
 let n = 8;
+let n1 = 0;
+let n2 = -1;
+let n3 = ''
+let n4 = 'test'
 
 test('is calcNatNumber correct', () => {
-    expect(algorithms.calcNatNumbers(arr)).toBe(2)
+    expect(algorithms.calcNatNumbers(str)).toBe(2)
 });
 
 test('input items are valid in calcNatNumbers', () => {
-    expect(algorithms.calcNatNumbers(arr1)).toBe(1)
+    expect(algorithms.calcNatNumbers(str1)).toBe(1)
 });
 
 test('is numbersAmount correct', () => {
-    expect(algorithms.numbersAmount(arr)).toBe(4)
+    expect(algorithms.numbersAmount(str)).toBe(4)
 });
 
 test('input items are valid in numbersAmount', () => {
-    expect(algorithms.numbersAmount(arr1)).toBe(0)
+    expect(algorithms.numbersAmount(str1)).toBe(0)
 });
 
 test('is pythagoreanTriplet correct', () => {
